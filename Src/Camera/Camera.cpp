b0670acffc378d1b0357d3e0e53a466edfde5b83
Tom Smith <1101282468@qq.com>
2019-02-22 22:46:54 +0800

    Remove files

--- Src/Camera/Camera.cpp
+++
@@ -1,6 +0,0 @@
-#include "Camera.hpp"
-
-namespace MoonLight
-{
-    Camera::Camera(const Vector3D_T<double> &p, const Vector3D_T<double> &f, const Vector3D_T<double> &u, const Vector3D_T<double> &r, const double a):position(p), front(f), up(u), right(r), aspect(a){}
-}
--- Src/Tracer/Tracer.cpp
+++
@@ -1,6 +0,0 @@
-#include "Tracer.hpp"
-
-namespace MoonLight
-{
-    std::vector<std::shared_ptr<Object<double>>> objects;
-}
