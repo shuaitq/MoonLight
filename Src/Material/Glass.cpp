cf2a7a1c5263ac77ee71378ca230eeeb051d84ac
Tom Smith <1101282468@qq.com>
2019-02-24 18:34:02 +0800

    Add Glass

--- Src/Material/Glass.cpp
+++ Src/Material/Glass.cpp
@@ -1,68 +1,57 @@
 #include "Glass.hpp"
 
 #include "../Utility/Rand.hpp"
+#include "../Core/World.hpp"
 
 namespace MoonLight
 {
-    Material Glass(const RGB_T<double> &color, const RGB_T<double> &emission, const BRDF &brdf, const double refractive_index);
+    Material Glass(const RGB_T<double> &color, const RGB_T<double> &emission, const BRDF &brdf, const double refractive_index)
     {
-        return [=](const Vector3D_T<double> &position, const Vector3D_T<double> &normal, const Vector3D_T<double> &incident const size_t depth)
+        return [=](const Vector3D_T<double> &position, const Vector3D_T<double> &normal, const Vector3D_T<double> &incident, const size_t depth)
         {
-            double idotn = Dot(normal, incident);
+            double idotn = Dot(incident, normal), k;
+            RGB_T<double> ret;
+            Vector3D_T<double> direction;
 
-            
-
-            Vector3D_T<double> n;
-            double nnt, ddn;
-            bool into;
-            if(Dot(normal, incident) < 0)
+            if(idotn <= 0)
             {
                 // into
-                into = true;
-                nnt = 1 / refractive_index;
-                n = normal;
+                double R0 = (refractive_index - 1) / (refractive_index + 1);
+                R0 *= R0;
+                double a = 1 + idotn;
+                double aa = a * a;
+                double Re = R0 + (1 - R0) * aa * aa * a;
+
+                double eta = 1 / refractive_index;
+                k = 1 - eta * eta * (1 - idotn * idotn);
+
+                ret = (emission + color * World::Get().tracer(brdf(position, normal, incident * eta - normal * (idotn * eta + sqrt(k))), depth)) * (1 - Re) + (emission + color * World::Get().tracer(brdf(position, normal, incident - (2 * idotn) * normal), depth)) * Re;
             }
             else
             {
                 // out
-                into = false;
-                nnt = refractive_index / 1;
-                n = -normal;
-            }
-            ddn = Dot(incident, n);
+                k = 1 - refractive_index * refractive_index * (1 - idotn * idotn);
 
-            double cos2t = 1-nnt*nnt*(1-ddn*ddn), a = refractive_index - 1, b = refractive_index + 1, c;
+                if(k < 0)
+                {
+                    // Total reflection
+                    ret = (emission + color * World::Get().tracer(brdf(position, normal, incident - (2 * idotn) * normal), depth));
+                }
+                else
+                {
+                    direction = incident * refractive_index + normal * (sqrt(k) - idotn * refractive_index);
 
-            if(cos2t < 0)
-            {
-                return Ray<double>(position, incident - 2 * Dot(incident, normal) * normal);
-                //return this->brdf->GetRay(position, n, incident - 2 * Dot(incident, normal) * normal);
-            }
+                    double R0 = (refractive_index - 1) / (refractive_index + 1);
+                    R0 *= R0;
+                    double a = 1 - Dot(normal, direction);
+                    double aa = a * a;
+                    double Re = R0 + (1 - R0) * aa * aa * a;
 
-            Vector3D_T<double> tdir = Normalize(incident * nnt - n * (ddn * nnt + sqrt(cos2t)));
-            if(into)
-            {
-                c = 1 + ddn;
+                    ret = (emission + color * World::Get().tracer(brdf(position, normal, direction), depth)) * (1 - Re) + (emission + color * World::Get().tracer(brdf(position, normal, incident - (2 * idotn) * normal), depth)) * Re;
+                }
             }
-            else
-            {
-                c = 1 - Dot(normal, tdir);
-            }
-
-            double R0 = a * a / (b * b);
-            double Re = R0 + (1 - R0) * c * c * c * c * c;
-
-            if(Rand() > Re)
-            {
-                // Refraction
-                return Ray<double>(position, tdir);
-            }
-            else
-            {
-                // Reflaction
-                return Ray<double>(position, incident - 2 * Dot(incident, normal) * normal);
-                //return this->brdf->GetRay(position, n, incident - 2 * Dot(incident, normal) * normal);
-            }
+            
+            return ret;
         };
     }
 }
